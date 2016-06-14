61b72010378b424603261061d7cbf8f26a555fb9
chrisp22 <user@users-MacBook-Pro.local>
2016-06-14 18:34:05 +0300

    update the xml

---
+++ app/src/main/java/ug/co/creativedna/android_beginner/MainActivity.java
@@ -0,0 +1,15 @@
+package ug.co.creativedna.android_beginner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newtest);
+    }
+
+
+}
